b15d2ad2e78d7f06034d42be2bdf0d24e800984f
KhaledHAnter <khaldntr11@gmail.com>
2024-02-22 03:50:21 +0200

    Provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app_th/Models/note_model.dart';
 import 'package:note_app_th/Views/edit_note_view.dart';
 import 'package:note_app_th/Views/note_view.dart';
 import 'package:note_app_th/constants.dart';
+import 'package:note_app_th/cubits/add_notes_cubit.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -18,17 +20,24 @@ class NoteApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+        ),
+        routes: {
+          NotesView.id: (context) => const NotesView(),
+          EditNoteView.id: (context) => const EditNoteView(),
+        },
+        initialRoute: NotesView.id,
       ),
-      routes: {
-        NotesView.id: (context) => const NotesView(),
-        EditNoteView.id: (context) => const EditNoteView(),
-      },
-      initialRoute: NotesView.id,
     );
   }
 }
